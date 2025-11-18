8a0f06f11023d38dd2095dec5be74b6311d18e68
LayanALorayj <layanAlorayj@outlook.com>
2025-11-18 10:14:00 +0300

    fixing

--- types/mui.d.ts
+++ types/mui.d.ts
@@ -1,1 +1,11 @@
-declare module "@mui/material/*";
+declare module '@mui/material/*';
+
+// declare module '@mui/material/CircularProgress';
+// declare module '@mui/material/Box';
+// declare module '@mui/material/Grid';
+// declare module '@mui/material/Typography';
+// declare module  '@mui/material/Container';
+// declare module '@mui/material';
+// declare module '@mui/material/styles'
+// declare module '@mui/material/CssBaseline'
+// declare module '@mui/material/Grid2'
